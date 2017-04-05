a218f383bd6eaaa435ed9c28805b8f2aa7d5edf7
capdilla <capdilla.cp@gmail.com>
2017-04-05 23:38:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# react-native-galery-carousel
+Galery carousel for react native
+
+
+
+## Demo
+
+![react-native-galery-carousel](https://media.giphy.com/media/3oKIPuWr6Mp7bOtYVG/giphy.gif)
+![react-native-galery-carousel](https://media.giphy.com/media/3oKIPj1guAZsLVqQfK/giphy.gif)
+
+
+## Usage
+
+```
+$ npm install --save react-native-galery-carousel
+```
+
+```javascript
+const imagesDemo = [
+    {
+        path: '/UYiroysl.jpg'
+    },
+    {  
+        path: '/UPrs1EWl.jpg'
+    },
+    {    
+        path: '/MABUbpDl.jpg'
+    },
+    {    
+        path: '/KZsmUi2l.jpg'
+    },
+    {    
+        path: '/2nCt3Sbl.jpg'
+    },
+    {    
+        path: '/lceHsT6l.jpg'
+    }
+];
+import GaleryCarousel from 'react-native-snap-carousel';
+
+    render () {
+      <View>
+        <GaleryCarousel
+          dataImage={imagesDemo}
+          joinFirst={'http://i.imgur.com'}
+          defaultImageIndex={2}
+          mainImageComponents={
+            <View style={{flex:1,backgroundColor:"transparent"}}>
+              <Text style={{color:"#fff"}}>Hello</Text>
+            </View>
+          }
+        />
+      </View>
+    }
+```
+
+## Props
+
+Prop | Description | Type | Default
+------ | ------ | ------ | ------
+**data** | the array of the image to show in the carousel | array | `[]`
+**getImageFrom** | the key of the array where you wanna get the image | string | `"path"`
+**joinFirst** | if you wanna join a link before **example** https://my-host-name.com/ `path` | string | `""`
+**joinLast** | if you wanna join string after **example** `path`/key="1255665" | string | `""`
+**defaultImageIndex** | index of the first image to show | Number | `1`
+**mainImageComponents** | if you wanna render a component in the Main image | Component | `<View/>`
+**mainHeight** | size of the galery carousel | Number | `280`
+
+
+## TODO
+- [ ] Implement 'onLongPress' in the scrollView images
---
+++ index.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import {View,Text,TouchableOpacity,ScrollView,Image,Dimensions } from "react-native"
+
+export default class GaleryCarousel extends Component{
+
+  constructor(props){
+    super(props)
+    this.width = Dimensions.get('window').width
+    this.state={
+      defaultImageIndex:this.props.defaultImageIndex,
+    }
+
+  }
+
+  changeImageIndex(defaultImageIndex){
+    this.setState({defaultImageIndex})
+  }
+
+  createImages(elt,key){
+    const {joinFirst,getImageFrom,joinLast} = this.props
+    return(
+      <TouchableOpacity
+        key={key}
+        style={{height: (this.props.mainHeight / 3.73)}}
+        onPress={()=>{this.changeImageIndex(key)} }
+        onLongPress={()=>console.log("long press ")}
+      >
+        <Image style={{height: (this.props.mainHeight / 3.73), width: 130}} source={{uri:`${joinFirst}${elt[getImageFrom]}${joinLast}`}} />
+      </TouchableOpacity>
+    )
+  }
+
+
+  render(){
+    const {dataImage:data,joinFirst,joinLast,getImageFrom,mainImageComponents} = this.props;
+    const {defaultImageIndex}= this.state;
+    if(data.length>0)
+      return(
+        <View>
+          <View style={{height: this.props.mainHeight, padding: 0}}>
+            <Image source={{uri:`${joinFirst}${data[defaultImageIndex][getImageFrom]}${joinLast}`}}
+             style={{width: this.width, height: this.props.mainHeight,flex:1}}
+             resizeMode='cover'>
+              {mainImageComponents}
+            </Image>
+          </View>
+          <View>
+            <ScrollView
+              ref={(scrollView) => { _scrollView = scrollView; }}
+              automaticallyAdjustContentInsets={false}
+              onScroll={() => { console.log('onScroll!'); }}
+              scrollEventThrottle={200}
+              horizontal={true}
+              style={{height: (this.props.mainHeight / 3.73)}}>
+              {data.map((elt, key) => this.createImages(elt, key))}
+            </ScrollView>
+          </View>
+        </View>
+      )
+    if(data.length==0)return (<View/>)
+  }
+}
+
+OwnCarousel.defaultProps={
+  data:[],
+  joinLast:'',
+  joinFirst:'',
+  defaultImageIndex:0,
+  mainHeight:280,
+  mainImageComponents:<View/>,
+  getImageFrom:"path"
+};
